b06bfc401f9c03953369a2c1610bbe9bfe5adcd1
Mar Mar <trespoderososminecraft@gmail.com>
2024-01-09 15:09:41 -0300

    tabs added to the mod

--- Enhanched_Minecraft_Mod/src/main/java/com/MarMar/Enhanced_Minecraft/Enhanced_Minecraft.java
+++ Enhanched_Minecraft_Mod/src/main/java/com/MarMar/Enhanced_Minecraft/Enhanced_Minecraft.java
@@ -1,35 +1,13 @@
 package com.MarMar.Enhanced_Minecraft;
 
-import com.MarMar.Enhanced_Minecraft.init.InitItems;
-import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
+import com.MarMar.Enhanced_Minecraft.items.InitItems;
+import com.MarMar.Enhanced_Minecraft.tabs.InitTabs;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.config.*;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
-import org.slf4j.Logger;
 
 
 @Mod(Enhanced_Minecraft.MOD_ID)
@@ -38,7 +16,18 @@ public class Enhanced_Minecraft
     public static final String MOD_ID = "enhanced_minecraft";
 
     public Enhanced_Minecraft(){
-        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-        InitItems.Items.register(bus);
+
+        IEventBus ModEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        InitTabs.Register(ModEventBus);
+        InitItems.Register(ModEventBus);
+
+//        ModEventBus.addListener(this::commonSetup);
+    //Register for creative tabs
+        MinecraftForge.EVENT_BUS.register(this);
+        ModEventBus.addListener(this::AddCreative);
+    }
+
+    private void AddCreative(BuildCreativeModeTabContentsEvent event){
     }
 }
