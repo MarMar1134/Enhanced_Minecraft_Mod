d643221143ac99fc232df216910b32238adfcdee
Mar Mar <trespoderososminecraft@gmail.com>
2024-07-11 22:16:00 -0300

    First beta! Added world gen
    the first beta is next to be launched so everyone can enjoy the Enhanced Minecraft Mod.

--- Enhanched_Minecraft_Mod/src/main/java/com/MarMar/Enhanced_Minecraft/event/ModEvents.java
+++ Enhanched_Minecraft_Mod/src/main/java/com/MarMar/Enhanced_Minecraft/event/ModEvents.java
@@ -35,19 +35,19 @@ public class ModEvents {
             trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(Items.EMERALD, 5),
                     new ItemStack(ModItems.Bronze_helmet.get(), 1),
-                    12, 1, 0.02f));
+                    8, 1, 0.2f));
             trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(Items.EMERALD, 9),
                     new ItemStack(ModItems.Bronze_chestplate.get(), 1),
-                    12, 1, 0.02f));
+                    8, 1, 0.2f));
             trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(Items.EMERALD, 7),
                     new ItemStack(ModItems.Bronze_leggings.get(), 1),
-                    12, 1, 0.02f));
+                    8, 1, 0.2f));
             trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(Items.EMERALD, 4),
                     new ItemStack(ModItems.Bronze_boots.get(), 1),
-                    12, 1, 0.02f));
+                    8, 1, 0.2f));
 
 
             //Level 2
@@ -60,22 +60,22 @@ public class ModEvents {
                     new ItemStack(Items.EMERALD, 5),
                     new ItemStack(ModItems.Saphire.get(), 2),
                     new ItemStack(Items.IRON_HELMET, 1),
-                    12, 12, 0.02f));
+                    6, 12, 0.4f));
             trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(Items.EMERALD, 9),
                     new ItemStack(ModItems.Saphire.get(), 5),
                     new ItemStack(Items.IRON_CHESTPLATE, 1),
-                    12, 12, 0.02f));
+                    6, 12, 0.4f));
             trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(Items.EMERALD, 7),
                     new ItemStack(ModItems.Saphire.get(), 3),
                     new ItemStack(Items.IRON_LEGGINGS, 1),
-                    12, 12, 0.02f));
+                    6, 12, 0.4f));
             trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(Items.EMERALD, 4),
                     new ItemStack(ModItems.Saphire.get(), 2),
                     new ItemStack(Items.IRON_BOOTS, 1),
-                    12, 12, 0.02f));
+                    6, 12, 0.4f));
 
             //Level 3
             trades.get(3).add((pTrader, pRandom) -> new MerchantOffer(
@@ -85,39 +85,39 @@ public class ModEvents {
             trades.get(3).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(ModItems.Saphire.get(), 5),
                     new ItemStack(ModItems.Steel_helmet.get(), 1),
-                    12, 8, 0.02f));
+                    4, 8, 0.2f));
             trades.get(3).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(ModItems.Saphire.get(), 9),
                     new ItemStack(ModItems.Steel_chestplate.get(), 1),
-                    12, 8, 0.02f));
+                    4, 8, 0.2f));
             trades.get(3).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(ModItems.Saphire.get(), 7),
                     new ItemStack(ModItems.Steel_leggings.get(), 1),
-                    12, 8, 0.02f));
+                    4, 8, 0.2f));
             trades.get(3).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(ModItems.Saphire.get(), 4),
                     new ItemStack(ModItems.Steel_boots.get(), 1),
-                    12, 8, 0.02f));
+                    4, 8, 0.2f));
 
             //Level 4
             trades.get(4).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(ModItems.Rubi.get(), 14),
                     new ItemStack(Items.DIAMOND_LEGGINGS, 1),
-                    2, 16, 0.02f));
+                    2, 16, 0.2f));
             trades.get(4).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(ModItems.Rubi.get(), 12),
                     new ItemStack(Items.DIAMOND_BOOTS, 1),
-                    2, 16, 0.02f));
+                    2, 16, 0.2f));
 
             //Level 5
             trades.get(5).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(ModItems.Rubi.get(), 16),
                     new ItemStack(Items.DIAMOND_HELMET, 1),
-                    2, 34, 0.02f));
+                    2, 34, 0.2f));
             trades.get(5).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(ModItems.Rubi.get(), 18),
                     new ItemStack(Items.DIAMOND_CHESTPLATE, 1),
-                    2, 34, 0.02f));
+                    2, 34, 0.2f));
         }
         if(event.getType() == VillagerProfession.TOOLSMITH){
             Int2ObjectMap<List<VillagerTrades.ItemListing>> trades =event.getTrades();
@@ -131,23 +131,27 @@ public class ModEvents {
                     new ItemStack(Items.EMERALD, 8),
                     new ItemStack(ModItems.Saphire.get(), 1),
                     12, 15, 0.02f));
+            trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(ModItems.Bronze_ingot.get(), 7),
+                    new ItemStack(Items.EMERALD, 1),
+                    12, 8, 0.02f));
                 //Bronze
             trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(Items.EMERALD, 9),
                     new ItemStack(ModItems.Bronze_axe.get(), 1),
-                    3, 10, 0.02f));
+                    3, 10, 0.2f));
             trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(Items.EMERALD, 11),
                     new ItemStack(ModItems.Bronze_pickaxe.get(), 1),
-                    3, 10, 0.02f));
+                    3, 10, 0.2f));
             trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(Items.EMERALD, 10),
                     new ItemStack(ModItems.Bronze_shovel.get(), 1),
-                    3, 10, 0.02f));
+                    3, 10, 0.2f));
             trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(Items.EMERALD, 7),
                     new ItemStack(ModItems.Bronze_hoe.get(), 1),
-                    3, 10, 0.02f));
+                    3, 10, 0.2f));
 
             //Level 3
             trades.get(3).add((pTrader, pRandom) -> new MerchantOffer(
@@ -159,41 +163,45 @@ public class ModEvents {
                     new ItemStack(Items.EMERALD, 6),
                     new ItemStack(ModItems.Saphire.get(), 3),
                     new ItemStack(Items.IRON_AXE, 1),
-                    3, 10, 0.02f));
+                    4, 10, 0.2f));
             trades.get(3).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(Items.EMERALD, 8),
                     new ItemStack(ModItems.Saphire.get(), 4),
                     new ItemStack(Items.IRON_PICKAXE, 1),
-                    3, 10, 0.02f));
+                    4, 10, 0.2f));
             trades.get(3).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(Items.EMERALD, 7),
                     new ItemStack(Items.IRON_SHOVEL, 5),
                     new ItemStack(ModItems.Saphire.get(), 2),
-                    3, 10, 0.02f));
+                    4, 10, 0.2f));
             trades.get(3).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(Items.EMERALD, 4),
                     new ItemStack(ModItems.Saphire.get(), 2),
                     new ItemStack(Items.IRON_HOE, 1),
-                    3, 10, 0.02f));
+                    4, 10, 0.2f));
                 //Steel
             trades.get(3).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(ModItems.Saphire.get(), 6),
                     new ItemStack(ModItems.Steel_axe.get(), 1),
-                    3, 10, 0.02f));
+                    5, 10, 0.2f));
             trades.get(3).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(ModItems.Saphire.get(), 8),
                     new ItemStack(ModItems.Steel_pickaxe.get(), 1),
-                    3, 10, 0.02f));
+                    5, 10, 0.2f));
             trades.get(3).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(ModItems.Saphire.get(), 7),
                     new ItemStack(ModItems.Steel_shovel.get(), 1),
-                    3, 10, 0.02f));
+                    5, 10, 0.2f));
             trades.get(3).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(ModItems.Saphire.get(), 4),
                     new ItemStack(ModItems.Steel_hoe.get(), 1),
-                    3, 10, 0.02f));
+                    5, 10, 0.2f));
 
             //Level 4
+            trades.get(4).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(ModItems.Steel_ingot.get(), 6),
+                    new ItemStack(ModItems.Saphire.get(), 1),
+                    12, 8, 0.02f));
             trades.get(4).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(ModItems.Saphire.get(), 7),
                     new ItemStack(ModItems.Rubi.get(), 1),
@@ -201,26 +209,96 @@ public class ModEvents {
             trades.get(4).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(ModItems.Rubi.get(), 9),
                     new ItemStack(Items.DIAMOND_AXE, 1),
-                    3, 15, 0.02f));
+                    2, 15, 0.2f));
             trades.get(4).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(ModItems.Rubi.get(), 7),
                     new ItemStack(Items.DIAMOND_SHOVEL, 1),
-                    3, 15, 0.02f));
+                    2, 15, 0.2f));
 
             //Level 5
             trades.get(5).add((pTrader, pRandom) -> new MerchantOffer(
-                    new ItemStack(ModItems.Rubi.get(), 32),
+                    new ItemStack(ModItems.Rubi.get(), 16),
                     new ItemStack(Items.DIAMOND, 1),
                     12, 45, 0.02f));
             trades.get(5).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(ModItems.Rubi.get(), 12),
                     new ItemStack(Items.DIAMOND_PICKAXE, 1),
-                    3, 15, 0.02f));
+                    2, 15, 0.2f));
             trades.get(5).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(ModItems.Rubi.get(), 6),
                     new ItemStack(Items.DIAMOND_HOE, 1),
-                    3, 15, 0.02f));
+                    2, 15, 0.2f));
+        }
+        if (event.getType() == VillagerProfession.WEAPONSMITH){
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades =event.getTrades();
+
+            trades.get(1).removeAll(trades.get(1));
+            trades.get(4).removeAll(trades.get(4));
+            trades.get(5).removeAll(trades.get(5));
+
+            //Level 1
+            trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.COAL, 15),
+                    new ItemStack(Items.EMERALD, 1),
+                    16, 1, 0.02f));
+            trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 4),
+                    new ItemStack(ModItems.Bronze_axe.get(), 1),
+                    6, 2, 0.2f));
+            trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 6),
+                    new ItemStack(ModItems.Bronze_sword.get(), 1),
+                    6, 2, 0.2f));
+
+            //Level 2
+            trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(ModItems.Saphire.get(), 2),
+                    new ItemStack(Items.COAL, 32),
+                    12, 8, 0.02f));
+            trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(ModItems.Bronze_ingot.get(), 5),
+                    new ItemStack(Items.EMERALD, 1),
+                    12, 8, 0.02f));
+
+            //Level 3
+            trades.get(3).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 6),
+                    new ItemStack(ModItems.Saphire.get(), 3),
+                    new ItemStack(Items.IRON_AXE, 1),
+                    4, 6, 0.2f));
+            trades.get(3).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 8),
+                    new ItemStack(ModItems.Saphire.get(), 4),
+                    new ItemStack(Items.IRON_SWORD, 1),
+                    4, 6, 0.2f));
+
+            //Level 4
+            trades.get(4).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(ModItems.Saphire.get(), 8),
+                    new ItemStack(ModItems.Rubi.get(), 1),
+                    12, 8, 0.02f));
+            trades.get(4).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 22),
+                    new ItemStack(ModItems.Rubi.get(), 1),
+                    12, 8, 0.02f));
+            trades.get(4).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(ModItems.Saphire.get(), 5),
+                    new ItemStack(ModItems.Steel_axe.get(), 1),
+                    4, 9, 0.2f));
+            trades.get(4).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(ModItems.Saphire.get(), 8),
+                    new ItemStack( ModItems.Steel_sword.get(), 1),
+                    4, 9, 0.2f));
+
+            //Level 5
+            trades.get(5).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(ModItems.Rubi.get(), 5),
+                    new ItemStack(Items.DIAMOND_AXE, 1),
+                    2, 9, 0.2f));
+            trades.get(5).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(ModItems.Rubi.get(), 8),
+                    new ItemStack(Items.DIAMOND_SWORD, 1),
+                    2, 9, 0.2f));
         }
     }
-
 }
---
+++ Enhanched_Minecraft_Mod/src/main/java/com/MarMar/Enhanced_Minecraft/worldgen/ModBiomeModifiers.java
@@ -0,0 +1,46 @@
+package com.MarMar.Enhanced_Minecraft.worldgen;
+
+import com.MarMar.Enhanced_Minecraft.Enhanced_Minecraft;
+import net.minecraft.core.HolderSet;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraftforge.common.world.BiomeModifier;
+import net.minecraftforge.common.world.ForgeBiomeModifiers;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModBiomeModifiers {
+    public static final ResourceKey<BiomeModifier> ADD_TIN_ORE = registerKey("add_tin_ore");
+    public static final ResourceKey<BiomeModifier> ADD_SILVER_ORE = registerKey("add_silver_ore");
+    public static final ResourceKey<BiomeModifier> ADD_SAPHIRE_ORE = registerKey("add_saphire_ore");
+    public static final ResourceKey<BiomeModifier> ADD_RUBI_ORE = registerKey("add_rubi_ore");
+
+    public static void bootstrap(BootstapContext<BiomeModifier> context) {
+        var placedFeatures = context.lookup(Registries.PLACED_FEATURE);
+        var biomes = context.lookup(Registries.BIOME);
+
+        context.register(ADD_TIN_ORE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+                biomes.getOrThrow(BiomeTags.IS_OVERWORLD),
+                HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.TIN_ORE_PLACED_KEY)),
+                GenerationStep.Decoration.UNDERGROUND_ORES));
+        context.register(ADD_SILVER_ORE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+                biomes.getOrThrow(BiomeTags.IS_OVERWORLD),
+                HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.SILVER_ORE_PLACED_KEY)),
+                GenerationStep.Decoration.UNDERGROUND_ORES));
+        context.register(ADD_SAPHIRE_ORE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+                biomes.getOrThrow(BiomeTags.IS_JUNGLE),
+                HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.SAPHIRE_ORE_PLACED_KEY)),
+                GenerationStep.Decoration.UNDERGROUND_ORES));
+        context.register(ADD_RUBI_ORE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+                biomes.getOrThrow(BiomeTags.IS_JUNGLE),
+                HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.RUBI_ORE_PLACED_KEY)),
+                GenerationStep.Decoration.UNDERGROUND_ORES));
+    }
+
+    private static ResourceKey<BiomeModifier> registerKey(String name) {
+        return ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS, new ResourceLocation(Enhanced_Minecraft.MOD_ID, name));
+    }
+}
